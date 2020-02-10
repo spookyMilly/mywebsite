5398702394b3bccbd66218738ad2ab2ea338e3ff
theodora.milona <theodora.milona@cosee.biz>
2020-02-10 18:50:59 +0100

    update layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,3 @@
 import React from "react"
 
-import LandingBio from "../components/landing-bio"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <LandingBio />
-  </Layout>
-)
-
-export default IndexPage
+export default () => <div>Hello world!</div>
